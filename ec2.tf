0a36612f91fbdbf28a34f9e565093639f6daa3ce
swathi <battulaswathi15407@gmail.com>
2026-07-05 01:53:25 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,11 @@
 module "ec2"  {
     source = "git::https://github.com/swathibattula-B/terraform-aws-instance1.git?ref=main"
     ami_id = "data.aws_ami.joindevops.id"
-    instance_type = "var.sg_ids"
-    tags = local.final_ec2_tags
+    instance_type = "var.instance_type"
+    sg_ids = var.sg_ids
+    project = var.project
+    environment = var.env
+    tags = var.ec2_tags
     
 } 
 
