c273750c2919237efff1e185aba2e8e73754abc0
swathi <battulaswathi15407@gmail.com>
2026-07-05 01:39:20 +0530

    terraform

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+module "ec2"  {
+    source = "git::https://github.com/swathibattula-B/terraform-aws-instance1.git?ref=main"
+    ami_id = "data.aws_ami.joindevops.id"
+    instance_type ="var.sg_ids
+    tags = local.final_ec2_tags
+    
+} 
+
---
+++ varibles.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    default = " "
+}
+
+variable "instance_type" {
+    default = " t3.micro"
+}
+
+variable "sg_ids" {
+    default = "sg-0b7ce8885eed0f473"
+}
