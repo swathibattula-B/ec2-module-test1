7427e18975a545df9e00d9ff4010d84d00b93f8e
swathi <battulaswathi15407@gmail.com>
2026-07-05 02:37:05 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,12 @@
 module "ec2"  {
     source = "git::https://github.com/swathibattula-B/terraform-aws-instance1.git?ref=main"
-    ami_id = "data.aws_ami.joindevops.id"
-    instance_type = "var.instance_type"
+    Project = var.project
+    Environment = var.env
+    ami_id = var.ami_id
     sg_ids = var.sg_ids
-    project = var.project
-    environment = var.env
-    
-    
-} 
+    ec2_tags = {
+        Name = "${var.project}-${var.env}"
+    }
+}
+  
 
--- varibles.tf
+++ varibles.tf
@@ -1,28 +1,18 @@
-variable "ami_id" {
-    default = " "
-}
-
-variable "instance_type" {
-    default = " t3.micro"
-}
-
-variable "sg_ids" {
-    default = "sg-0b7ce8885eed0f473"
-}
-
 variable "project" {
     default = "roboshop"
 }
 
-variable "environment" {
-    default = "dev"
+variable "env" {
+    default = "env"
 }
 
-variable "ec2_tags" {
-    type = map
-    default = {
-        Name = "terraform"
-        Project = "roboshop"
-        Environment = "dev"
-    }
+variable  "ami_id"   {
+    default = 
 }
+
+variable "sg_ids"  {
+    default = ""
+}
+
+
+
